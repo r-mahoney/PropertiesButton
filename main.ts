83ca9ccf8a8cb3468695e3f08043beba46cd44c1
Ryan Mahoney <rsmahoney211@gmail.com>
2023-08-02 14:25:08 -0500

    changed language from frontmatter to properties throughout plugin

--- main.ts
+++ main.ts
@@ -7,21 +7,21 @@ import {
 	Setting,
 } from "obsidian";
 
-interface FrontmatterPluginSettings {
+interface PropertiesPluginSettings {
 	showButton: boolean;
 }
 
-const DEFAULT_SETTINGS: FrontmatterPluginSettings = {
+const DEFAULT_SETTINGS: PropertiesPluginSettings = {
 	showButton: true,
 };
 
 const ROOT_WORKSPACE_CLASS = ".mod-vertical.mod-root";
 
-export default class FrontmatterPlugin extends Plugin {
-	settings: FrontmatterPluginSettings;
+export default class PropertiesPlugin extends Plugin {
+	settings: PropertiesPluginSettings;
 	windowSet: Set<Window> = new Set();
 
-	private createFrontmatterElement(
+	private createPropertiesElement(
 		config: {
 			id: string;
 			className: string;
@@ -38,6 +38,7 @@ export default class FrontmatterPlugin extends Plugin {
 		button.setClass("buttonItem").onClick(fn);
 
 		button.buttonEl.innerHTML = config.icon;
+		// button.setIcon(config.icon)
 
 		let curWindow = config.curWindow || window;
 
@@ -50,12 +51,11 @@ export default class FrontmatterPlugin extends Plugin {
 
 	public createElements(window?: Window) {
 		const { showButton } = this.settings;
-		const active = this.app.workspace.getActiveViewOfType(MarkdownView);
 		if (showButton) {
-			this.createFrontmatterElement(
+			this.createPropertiesElement(
 				{
-					id: "_frontmatterButton",
-					className: "frontmatterButton",
+					id: "_propertiesButton",
+					className: "propertiesButton",
 					icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M21 9L17 9" stroke="black" stroke-width="2" stroke-linecap="round"/>
 					<path d="M21 15L17 15" stroke="black" stroke-width="2" stroke-linecap="round"/>
@@ -95,7 +95,7 @@ export default class FrontmatterPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-		this.addSettingTab(new FrontmatterSettingsTab(this.app, this));
+		this.addSettingTab(new PropertiesSettingsTab(this.app, this));
 		this.app.workspace.onLayoutReady(() => {
 			this.createElements();
 		});
@@ -117,7 +117,7 @@ export default class FrontmatterPlugin extends Plugin {
 						.querySelector(".inline-title")
 						?.addEventListener("mouseenter", () => {
 							const button =
-								document.getElementById("_frontmatterButton");
+								document.getElementById("_propertiesButton");
 							if (button) {
 								active!.contentEl
 									.querySelector(".cm-sizer")
@@ -136,7 +136,7 @@ export default class FrontmatterPlugin extends Plugin {
 								this.createElements();
 								const button =
 									document.getElementById(
-										"_frontmatterButton"
+										"_propertiesButton"
 									);
 								if (button) {
 									active!.contentEl
@@ -152,7 +152,7 @@ export default class FrontmatterPlugin extends Plugin {
 						.querySelector(".inline-title")
 						?.addEventListener("mouseleave", () => {
 							const button =
-								document.getElementById("_frontmatterButton");
+								document.getElementById("_propertiesButton");
 							if (button) {
 								button.style.opacity = "0";
 							}
@@ -163,7 +163,7 @@ export default class FrontmatterPlugin extends Plugin {
 	}
 
 	onunload() {
-		this.removeButton("_frontmatterButton");
+		this.removeButton("_propertiesButton");
 	}
 
 	async loadSettings() {
@@ -179,10 +179,10 @@ export default class FrontmatterPlugin extends Plugin {
 	}
 }
 
-class FrontmatterSettingsTab extends PluginSettingTab {
-	plugin: FrontmatterPlugin;
+class PropertiesSettingsTab extends PluginSettingTab {
+	plugin: PropertiesPlugin;
 
-	constructor(app: App, plugin: FrontmatterPlugin) {
+	constructor(app: App, plugin: PropertiesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -195,18 +195,18 @@ class FrontmatterSettingsTab extends PluginSettingTab {
 	display(): void {
 		const { containerEl } = this;
 		containerEl.empty();
-		containerEl.createEl("h2", { text: "Frontmatter Button Settings" });
+		containerEl.createEl("h2", { text: "Toggle Properties Button Settings" });
 
 		new Setting(containerEl)
-			.setName("Show frontmatter button")
-			.setDesc("Show frontmatter button above note title")
+			.setName("Show Toggle Properties Button")
+			.setDesc("Show toggle properties button above note title")
 			.addToggle((value) =>
 				value
 					.setValue(this.plugin.settings.showButton)
 					.onChange(async (value) => {
 						this.plugin.settings.showButton = value;
 						await this.plugin.saveSettings();
-						this.rebuildElements("_frontmatterButton");
+						this.rebuildElements("_propertiesButton");
 					})
 			);
 	}
